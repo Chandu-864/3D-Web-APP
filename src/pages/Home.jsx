6053b1d5657194d1e422a43fce6cbe9bb3b53714
Chandu Thalati <141419083+Chandu-864@users.noreply.github.com>
2025-01-15 13:00:01 -0500

    Updated sound image position

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -97,7 +97,7 @@ const Home = () => {
 
             </Suspense>
         </Canvas>
-        <div className="absolute bottom-20 left-2 p-1">
+        <div className="absolute bottom-20 left-2 p-2">
             <img
             src={!playMusic ? soundoff : soundon}
             alt='jukebox'
